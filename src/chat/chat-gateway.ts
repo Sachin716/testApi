da6c5a2d858da780b44a1fad08fea4afe8708237
Sachin716 <97391644+Sachin716@users.noreply.github.com>
2024-10-06 00:41:47 +0530

    Update chat-gateway.ts

--- src/chat/chat-gateway.ts
+++ src/chat/chat-gateway.ts
@@ -1,7 +1,7 @@
 import { MessageBody, OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
 import { Socket, Server } from "socket.io";
 
-@WebSocketGateway({
+@WebSocketGateway(3000 , {
     cors: {
         origin: "*"
     }
