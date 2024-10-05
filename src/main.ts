e65f617e5fa2daf15b827a1f1ae3f3ab8c8689bd
Sachin716 <97391644+Sachin716@users.noreply.github.com>
2024-10-06 00:47:35 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: '*', // Specify your frontend URL
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, // Include credentials if needed
   });
   
-  const app = await NestFactory.create(AppModule);
+  
   await app.listen(3000);
 }
 bootstrap();
